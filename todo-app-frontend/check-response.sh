3e121b4779cb9aa96a85e80f4847caede4114de7
Ananthu Subramanian <anandusubramanian@gmail.com>
2023-05-20 15:51:23 +0530

    Testing

--- todo-app-frontend/check-response.sh
+++ todo-app-frontend/check-response.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-response=$(curl -s -o /dev/null -w "%{http_code}" -H "Accept: application/json" --head http://localhost:3000)
+response=$(curl -s -o /dev/null -w "%{http_code}" -H "Accept: application/json" --head http://localhost:3000/)
 if [[ $response == "200" ]]; then
     echo "Request succeeded. Proceed with building the image."
 else
